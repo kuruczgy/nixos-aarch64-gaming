d7eaa8bea341612839acde57c7dd2f100861eb1b
György Kurucz <me@kuruczgy.com>
2024-11-29 23:13:13 +0100

    wip: vm setup working at a basic level now

--- flake.nix
+++ flake.nix
@@ -1,21 +1,31 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
   };
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, home-manager }: {
     nixosConfigurations.vm = nixpkgs.lib.nixosSystem {
       system = "aarch64-linux";
       modules = [
+        home-manager.nixosModules.home-manager
         ({ pkgs, modulesPath, ... }: {
           nixpkgs.overlays = [
             (final: prev: {
               qemu_kvm = prev.qemu_kvm.overrideAttrs {
                 version = "9.1.50";
+                # version = "8.2.92";
                 src = final.fetchFromGitLab {
                   domain = "gitlab.freedesktop.org";
-                  owner = "digetx";
                   repo = "qemu";
+
+                  owner = "digetx";
                   rev = "refs/heads/native-context-v4";
+                  hash = "sha256-ptC7SUFzNgkRoyOEZqf6JbSGmR/VuxYLPomVf4SRbQQ=";
+
+                  # owner = "robclark";
+                  # rev = "refs/heads/wip";
+                  # hash = "sha256-sK5MUjROlVplUJtmV0mIn80vWLCgRNv9VHSW80UGrqY=";
+
                   forceFetchGit = true;
                   postFetch = ''
                     cd $out
@@ -25,23 +35,56 @@
                     done
                     rm -r subprojects/*/.git
                   '';
-                  hash = "sha256-ptC7SUFzNgkRoyOEZqf6JbSGmR/VuxYLPomVf4SRbQQ=";
                 };
-                patches = [ ];
+                patches = [
+                  # (final.fetchpatch {
+                  #   url = "https://gitlab.freedesktop.org/digetx/qemu/-/commit/7d4b81f9497658f86d195569c649f306e76deb53.patch";
+                  #   hash = "sha256-aUYr2RShclZhdmZvgDoktSr2mgXEqV47+p1w8wTOLFM=";
+                  # })
+                ];
               };
+
               virglrenderer = prev.virglrenderer.overrideAttrs (old: {
                 mesonFlags = old.mesonFlags ++ [
                   (nixpkgs.lib.mesonOption "drm-renderers" "msm")
                 ];
               });
+
+              # mesa-guest = prev.mesa.overrideAttrs (old: {
+              mesa = prev.mesa.overrideAttrs (old: {
+                mesonFlags = old.mesonFlags ++ [
+                  (nixpkgs.lib.mesonOption "freedreno-kmds" "msm,virtio")
+                ];
+                patches = old.patches ++ [
+                  (final.fetchpatch {
+                    url = "https://gitlab.freedesktop.org/mesa/mesa/-/commit/d71c63d7dfa9a46bbf8012bb7ad3262971d627de.patch";
+                    hash = "sha256-F4KSy+lTsJfTizpmDNZGOGEvfUx/MLMmjUYLLhDixjs=";
+                  })
+                  (final.fetchpatch {
+                    url = "https://gitlab.freedesktop.org/mesa/mesa/-/commit/2757fa4dca62ff50404611ca3d180defcdb80e32.patch";
+                    hash = "sha256-pH7vHRNZvSYczLY3ajtxZyG6DN37gimyZi+cI0KRdeE=";
+                  })
+                ];
+              });
             })
           ];
 
+          # system.replaceDependencies.replacements = [{
+          #   oldDependency = pkgs.mesa;
+          #   newDependency = pkgs.mesa-guest;
+          # }];
+          # hardware.graphics.package = pkgs.mesa-guest.drivers;
+
           imports = [ (modulesPath + "/virtualisation/qemu-vm.nix") ];
           virtualisation = {
+            memorySize = 4096;
+            cores = 4;
             qemu.options = [
+              "-device virtio-sound"
               "-device virtio-gpu-gl,stats=off,blob=on,hostmem=32G,drm_native_context=on"
-              "-display gtk,show-tabs=on,gl=on"
+              # "-device virtio-gpu-gl,stats=off,blob=on,hostmem=32G"
+              # "-display gtk,show-tabs=on,gl=on"
+              "-display sdl,gl=on"
             ];
           };
           hardware.graphics.enable = true;
@@ -60,7 +103,51 @@
 
           environment.systemPackages = with pkgs; [
             vulkan-tools
+            kmscube
+            evtest
+            pavucontrol
+            glxinfo
+            firefox
+            minetest
+            superTuxKart
+            openttd
+            zeroad
+            xonotic
           ];
+
+          # Enable sound with pipewire
+          security.rtkit.enable = true;
+          services.pipewire = {
+            enable = true;
+            alsa.enable = true;
+            alsa.support32Bit = true;
+            pulse.enable = true;
+          };
+
+          # services.xserver.enable = true;
+          # services.xserver.displayManager.gdm.enable = true;
+          # services.xserver.desktopManager.gnome.enable = true;
+
+          programs.sway = {
+            enable = true;
+            wrapperFeatures.gtk = true;
+          };
+          services.dbus.enable = true;
+
+          environment.shellAliases = {
+            s = "WLR_RENDERER=vulkan WLR_NO_HARDWARE_CURSORS=1 sway";
+          };
+
+          home-manager.users.user = {
+            home.stateVersion = "24.11";
+            wayland.windowManager.sway = {
+              enable = true;
+              package = pkgs.sway;
+              systemd.enable = true;
+              config.modifier = "Mod1";
+              # config.output."*".scale = "2";
+            };
+          };
         })
       ];
     };
