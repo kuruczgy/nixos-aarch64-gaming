e2e2929d69b91c3cfed8c2f942cedd893d2d834d
György Kurucz <me@kuruczgy.com>
2024-12-01 11:38:19 +0100

    add fex-emu patches

---
+++ packages/fex-emu.nix
@@ -0,0 +1,38 @@
+{ pkgs, fetchFromGitHub }:
+
+let
+  clangStdEnv = pkgs.stdenvAdapters.overrideCC pkgs.llvmPackages.stdenv (
+    pkgs.llvmPackages.clang.override {
+      bintools = pkgs.llvmPackages.bintools;
+    }
+  );
+in
+clangStdEnv.mkDerivation {
+  name = "fex-emu";
+  src = fetchFromGitHub {
+    repo = "FEX";
+    owner = "FEX-Emu";
+    rev = "22058c06a1f3585f66ed71765c9b6b652bdd66cc";
+    hash = "sha256-Spn75tiYJmweZNbaYeR6u/omjMPyC2nBuz8SIQCZGGE=";
+    fetchSubmodules = true;
+  };
+  patches = [ ./fex-emu-fixes.patch ];
+  nativeBuildInputs = with pkgs; [
+    git
+    cmake
+    ninja
+    pkg-config
+    nasm
+    (python3.withPackages (ps: with ps; [
+      clang
+      setuptools
+    ]))
+  ];
+  buildInputs = with pkgs; [
+    openssl
+    qt5.qtbase
+    qt5.qtdeclarative
+    qt5.qtquickcontrols
+    qt5.qtquickcontrols2
+  ];
+}
